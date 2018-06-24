8faacd6b8f96182f65936dfe374311faaaff47d2
Bhushan <bhushan29.mohite@gmail.com>
2018-06-24 19:16:01 -0400

    Navbar updates

---
+++ src/app/advertise-editor/advertise-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advertise-editor',
+  templateUrl: './advertise-editor.component.html',
+  styleUrls: ['./advertise-editor.component.css']
+})
+export class AdvertiseEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
