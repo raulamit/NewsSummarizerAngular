7804f444a7403dba1af6a8d97fc0d54c6353fa46
Bhushan <bhushan29.mohite@gmail.com>
2018-06-26 00:54:51 -0400

    heroku java server updated

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -1,5 +1,5 @@
-export const JAVA_SERVER_URL = 'http://localhost:8080';
-// export const JAVA_SERVER_URL = 'https://newssummarizer-webdev2018-java.herokuapp.com/';
+// export const JAVA_SERVER_URL = 'http://localhost:8080';
+export const JAVA_SERVER_URL = 'https://newssummarizer-webdev2018-java.herokuapp.com/';
 export const ADVERTISER = 'Advertiser';
 export const EDITOR = 'Editor';
 export const READER = 'Reader';
